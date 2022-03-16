769a1fb087d35fbffeec10f013973d6c08c5fd11
Dardaul Hoque <rudro1152@gmail.com>
2022-03-16 13:05:08 +0600

    Restructured Node class

--- node.mjs
+++ node.mjs
@@ -1,3 +1,4 @@
+const featureLimit = 7;
 const splitCount = 5;
 const splitCombinations = [];
 
@@ -8,18 +9,35 @@ for (let i = 1; i < splitCount; ++i) {
     }
 }
 
-
 export default class Node {
-    constructor (range) {
-        const {min, max} = range;
-        const unitDistance = (max - min) / splitCount;
+    constructor (featureIndex, wineList) {
+        let featureValue = {
+            min: Infinity,
+            max: -Infinity
+        };
+
+        wineList.forEach(wine => {
+            featureValue.min = Math.min(featureValue.min, wine[featureIndex]);
+            featureValue.max = Math.max(featureValue.max, wine[featureIndex]);     
+        });
         
+        const unitDistance = (featureValue.max - featureValue.min) / splitCount;
+        
+        this.wineList = wineList;
+        this.featureIndex = featureIndex;
         this.splits = splitCombinations.map(
             pair => ({ 
-                lowerBound: min + pair[0] * unitDistance,
-                upperBound: min + pair[1] * unitDistance
+                lowerBound: featureValue.min + pair[0] * unitDistance,
+                upperBound: featureValue.min + pair[1] * unitDistance
             })
         );
-
     }    
+    
+    calcOptimumSplit() {
+        
+    }
+    
+    calcEntropy() {
+        
+    }
 }
--- train.mjs
+++ train.mjs
@@ -1,20 +1,5 @@
-import data from './data.mjs';
+import dataSet from './data.mjs';
 import Node from './node.mjs';
 
-const featureLimit = 7;
-const nodeArray = [];
-
-// feature engineering 
-for (let featureIndex = 1; featureIndex <= featureLimit; ++featureIndex) {  // Index 0 holds the predermined class
-    let range = {
-        min: Infinity,
-        max: -Infinity
-    };
-
-    data.forEach(features => {
-        range.min = Math.min(range.min, features[featureIndex]);
-        range.max = Math.max(range.max, features[featureIndex]);     
-    });
-    
-    new Node(range);
-}
+// training
+new Node(1, dataSet);
